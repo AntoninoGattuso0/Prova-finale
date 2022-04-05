cbf120c102729d2f052f6ea359501e58e1213eff
rebecamersam <rebecafrancesca.mersam>
2022-04-05 20:23:51 +0200

    add class

---
+++ src/main/java/it/polimi/ingsw/model/Rebi/DiningRoom.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.model.Rebi;
+
+import it.polimi.ingsw.model.Paul.ColorPawn;
+
+public class DiningRoom {
+    private int blueRow;
+    private int greenRow;
+    private int yellowRow;
+    private int pinkRow;
+    private int redRow;
+
+    public void addPawn(ColorPawn colorPawn) {
+
+    }
+    public void coinPosition(ColorPawn colorPawn){
+
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/model/Rebi/Entrance.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.model.Rebi;
+
+public class Entrance {
+}
