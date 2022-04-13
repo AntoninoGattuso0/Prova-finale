c5bf37e734d431bec2e978909c03835c1cd50b2e
AntoninoGattuso0 <antonino.gattuso@mail.polimi.it>
2022-04-13 16:50:03 +0200

    ModificaClassi

--- src/main/java/it/polimi/ingsw/model/Cloud.java
+++ src/main/java/it/polimi/ingsw/model/Cloud.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.model;
 
+import java.util.*;
+
 public class Cloud {
     private int numPawn;
     private int greenPawn;
@@ -7,9 +9,77 @@ public class Cloud {
     private int yellowPawn;
     private int pinkPawn;
     private int bluePawn;
-    public void getNumPlayer(){
+
+    public void getNumPlayer() {
         int numPawn;
     }
+
     public void refillCloud() {
+        int i;
+        Random rnd = new Random();
+        Map<Integer, String> m = new HashMap<Integer, String>();
+        m.put(0, "GREEN");
+        m.put(1, "RED");
+        m.put(2, "YELLOW");
+        m.put(3, "PINK");
+        m.put(4, "BLUE");
+        ArrayList<String> startingPawn = new ArrayList<>();
+        if (StudentBag.greenNum != 0)
+            startingPawn.add("GREEN");
+        if (StudentBag.redNum != 0)
+            startingPawn.add("RED");
+        if (StudentBag.yellowNum != 0)
+            startingPawn.add("YELLOW");
+        if (StudentBag.pinkNum != 0)
+            startingPawn.add("PINK");
+        if (StudentBag.blueNum != 0)
+            startingPawn.add("BLUE");
+        for (i = 0; i < numPawn; i++) {
+            if (StudentBag.num > 0) {
+                int random = rnd.nextInt(startingPawn.size());
+                String color = m.get(random);
+                if (Objects.equals(startingPawn.get(random), "GREEN")) {
+                    greenPawn++;
+                    numPawn++;
+                    StudentBag.num--;
+                    StudentBag.greenNum--;
+                    if(StudentBag.greenNum==0){
+                        startingPawn.remove(random);
+                    }
+                } else if (Objects.equals(startingPawn.get(random), "RED")) {
+                    redPawn++;
+                    numPawn++;
+                    StudentBag.num--;
+                    StudentBag.redNum--;
+                    if(StudentBag.redNum==0){
+                        startingPawn.remove(random);
+                    }
+                } else if (Objects.equals(startingPawn.get(random), "YELLOW")) {
+                    yellowPawn++;
+                    numPawn++;
+                    StudentBag.num--;
+                    StudentBag.yellowNum--;
+                    if(StudentBag.yellowNum==0){
+                        startingPawn.remove(random);
+                    }
+                } else if (Objects.equals(startingPawn.get(random), "PINK")) {
+                    pinkPawn++;
+                    numPawn++;
+                    StudentBag.num--;
+                    StudentBag.pinkNum--;
+                    if(StudentBag.pinkNum==0){
+                        startingPawn.remove(random);
+                    }
+                } else if (Objects.equals(startingPawn.get(random), "BLUE")) {
+                    bluePawn++;
+                    numPawn++;
+                    StudentBag.num--;
+                    StudentBag.blueNum--;
+                    if(StudentBag.blueNum==0){
+                        startingPawn.remove(random);
+                    }
+                }
+            }
+        }
     }
 }
