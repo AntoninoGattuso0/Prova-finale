de35b90018b7263c99d02f034b810702fed2d0c0
rebecamersam <rebecafrancesca.mersam>
2022-04-13 17:28:09 +0200

    add classes

--- src/main/java/it/polimi/ingsw/model/Entrance.java
+++ src/main/java/it/polimi/ingsw/model/Entrance.java
@@ -3,24 +3,34 @@ package it.polimi.ingsw.model;
 import it.polimi.ingsw.model.ColorPawn;
 
 public class Entrance {
-    protected int bluePawn;
-    protected int greenPawn;
-    protected int yellowPawn;
-    protected int pinkPawn;
-    protected int redPawn;
+
     protected int numPawn;
 
     //in Game-start bisogna aggiungere un metodo che mette in Entrance le pedine studenti in base al numero di Player
 
-    public void checkNum(int numPawn){
+    //DEVO AGGIUNGERE METODO CHOOSE CLOUD PER REFILL ENTRATA
+    //modifiche all'UML:ho aggiunto setter per definire quante sono le torri iniziali, final perché si setta all'inizio
+    public final void setNumPawn(){
+        if(Game.totPlayer == 2 || Game.totPlayer == 4) numPawn = 9;
+        else if(Game.totPlayer == 3) numPawn = 6;
+    }
 
+    //modifiche all'UML: il ritorno di check è boolean
+    public boolean checkNum(){
+        if(Game.totPlayer == 2 || Game.totPlayer == 4 && numPawn > 4)
+            return true;
+        else if(Game.totPlayer = 3 && numPawn > 5)
+            return true:
+        else return false;
     }
 
     public void moveToDining(ColorPawn colorPawn){
 
+
+
     }
 
-    public void moveToIsland(ColorPawn colorPawn){
+    public void moveToIsland(){
 
     }
 }
