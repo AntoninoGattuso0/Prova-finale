c5f9623a74e5eeeb7523330f520e6c687c8980a0
rebecamersam <rebecafrancesca.mersam>
2022-04-21 20:13:21 +0200

    add classes

--- src/main/java/it/polimi/ingsw/model/DiningRoom.java
+++ src/main/java/it/polimi/ingsw/model/DiningRoom.java
@@ -3,17 +3,19 @@ package it.polimi.ingsw.model;
 import java.util.Objects;
 
 public class DiningRoom {
-    //modifiche all'UML: array di array invece che 5 int
+    //modifiche all'UML: array di array: in cui gli elementi sono 0 o 1;
+    //5 int che tengono conto di quante pedine di ogni colore sono presenti nella sala
     int[][] position = new int[5][10];
-    int numGreen = 0;
-    int numRed = 0;
-    int numYellow = 0;
-    int numPink = 0;
-    int numBlue = 0;
+    protected int numGreen = 0;
+    protected int numRed = 0;
+    protected int numYellow = 0;
+    protected int numPink = 0;
+    protected int numBlue = 0;
 
-    public void addPawn(ColorPawn colorPawn, int position[][], Player player) {
+    public void addPawn(ColorPawn colorPawn, Player player) {
+
         int j; //variabile che serve a iterare dentro le righe della sala
-        if(Objects.equals(colorPawn, Game.m.get(0)))
+        if(Objects.equals(colorPawn.toString(), Game.m.get(0)))
         for(j = 0; j < 10; j++){
                  if(position[0][j] != 1){
                    position[0][j]= 1;
@@ -24,7 +26,7 @@ public class DiningRoom {
                  }
              }
 
-        if(Objects.equals(colorPawn, Game.m.get(1)))
+        if(Objects.equals(colorPawn.toString(), Game.m.get(1)))
             for(j = 0; j < 10; j++){
                 if(position[1][j] != 1){
                     position[1][j]= 1;
@@ -35,7 +37,7 @@ public class DiningRoom {
                 }
             }
 
-        if(Objects.equals(colorPawn, Game.m.get(2)))
+        if(Objects.equals(colorPawn.toString(), Game.m.get(2)))
             for(j = 0; j < 10; j++){
                 if(position[2][j] != 1){
                     position[2][j]= 1;
@@ -46,7 +48,7 @@ public class DiningRoom {
                 }
             }
 
-        if(Objects.equals(colorPawn, Game.m.get(3)))
+        if(Objects.equals(colorPawn.toString(), Game.m.get(3)))
             for(j = 0; j < 10; j++){
                 if(position[3][j] != 1){
                     position[3][j]= 1;
@@ -57,7 +59,7 @@ public class DiningRoom {
                 }
             }
 
-        if(Objects.equals(colorPawn, Game.m.get(4)))
+        if(Objects.equals(colorPawn.toString(), Game.m.get(4)))  //metodo toString restituisce la rappresentazione di stringa delle costanti enum
             for(j = 0; j < 10; j++){
                 if(position[4][j] != 1){
                     position[4][j]= 1;
--- src/main/java/it/polimi/ingsw/model/ProfTable.java
+++ src/main/java/it/polimi/ingsw/model/ProfTable.java
@@ -1,16 +1,17 @@
 package it.polimi.ingsw.model;
 
 public class ProfTable {
-    private int blueProf = -1;
-    private int greenProf = -1;
-    private int yellowProf = -1;
-    private int pinkProf = -1;
-    private int redProf = -1;
+
+    protected boolean[]
+    protected int blueProf = -1;
+    protected int greenProf = -1;
+    protected int yellowProf = -1;
+    protected int pinkProf = -1;
+    protected int redProf = -1;
 
 
     //modifiche all'UML:metodo restituisce un int ovvero il numero del giocatore che possiede il professore
-    public static int checkProf(int color)  { //allora dovrebbe ricevere in ingresso un int che indica il colore --Paul
-
-    }
-
+    public int checkProf(int color)  { //allora dovrebbe ricevere in ingresso un int che indica il colore --Paul
+//aspetto che sia implementato il metodo che definisce chi ha il prof
+        }
 }
