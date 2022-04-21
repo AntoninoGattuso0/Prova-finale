92a2f135a59372b1c5cf4c3f81d29c9f770f0280
rebecamersam <rebecafrancesca.mersam>
2022-04-21 18:19:00 +0200

    add classes

--- src/main/java/it/polimi/ingsw/model/DiningRoom.java
+++ src/main/java/it/polimi/ingsw/model/DiningRoom.java
@@ -7,13 +7,42 @@ public class DiningRoom {
     int[][] position = new int[5][10];
 
     public void addPawn(ColorPawn colorPawn, int position[][]){
+        int j;
         if(Objects.equals(colorPawn, Game.m.get(0)))
-        for(int j = 0; j < 10; j++){
+        for(j = 0; j < 10; j++){
                  if(position[0][j] != 1){
                    position[0][j]= 1;
                    break;
                  }
              }
+        if(Objects.equals(colorPawn, Game.m.get(1)))
+            for(j = 0; j < 10; j++){
+                if(position[1][j] != 1){
+                    position[1][j]= 1;
+                    break;
+                }
+            }
+        if(Objects.equals(colorPawn, Game.m.get(2)))
+            for(j = 0; j < 10; j++){
+                if(position[2][j] != 1){
+                    position[2][j]= 1;
+                    break;
+                }
+            }
+        if(Objects.equals(colorPawn, Game.m.get(3)))
+            for(j = 0; j < 10; j++){
+                if(position[3][j] != 1){
+                    position[3][j]= 1;
+                    break;
+                }
+            }
+        if(Objects.equals(colorPawn, Game.m.get(4)))
+            for(j = 0; j < 10; j++){
+                if(position[4][j] != 1){
+                    position[4][j]= 1;
+                    break;
+                }
+            }
     }
 
 
