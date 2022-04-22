779e33dade7b16cbc4f12986f497481c2df4856e
rebecamersam <rebecafrancesca.mersam>
2022-04-22 11:40:43 +0200

    add classes

--- src/main/java/it/polimi/ingsw/model/DiningRoom.java
+++ src/main/java/it/polimi/ingsw/model/DiningRoom.java
@@ -21,7 +21,7 @@ public class DiningRoom {
                    position[0][j]= 1;
                    numGreen = j;
                    entrance.greenPawn--;
-                     if(Game.isExpert && (position[0][2] == 1 || position[0][5] == 1 || position[0][9] == 1))
+                     if(Game.isExpert && (position[0][2] == 1 || position[0][5] == 1 || position[0][8] == 1))
                          player.numCoin++;
                    break;
                  }
@@ -33,7 +33,7 @@ public class DiningRoom {
                     position[1][j]= 1;
                     numRed = j;
                     entrance.redPawn--;
-                    if(Game.isExpert && (position[1][2] == 1 || position[1][5] == 1 || position[1][9] == 1))
+                    if(Game.isExpert && (position[1][2] == 1 || position[1][5] == 1 || position[1][8] == 1))
                         player.numCoin++;
                     break;
                 }
@@ -45,7 +45,7 @@ public class DiningRoom {
                     position[2][j]= 1;
                     numYellow = j;
                     entrance.yellowPawn--;
-                    if(Game.isExpert && (position[2][2] == 1 || position[2][5] == 1 || position[2][9] == 1))
+                    if(Game.isExpert && (position[2][2] == 1 || position[2][5] == 1 || position[2][8] == 1))
                         player.numCoin++;
                     break;
                 }
@@ -57,7 +57,7 @@ public class DiningRoom {
                     position[3][j]= 1;
                     numPink = j;
                     entrance.pinkPawn--;
-                    if(Game.isExpert && (position[3][2] == 1 || position[3][5] == 1 || position[3][9] == 1))
+                    if(Game.isExpert && (position[3][2] == 1 || position[3][5] == 1 || position[3][8] == 1))
                         player.numCoin++;
                     break;
                 }
@@ -67,9 +67,9 @@ public class DiningRoom {
             for(j = 0; j < 10; j++){
                 if(position[4][j] != 1){
                     position[4][j]= 1;
-                    numBlue = j;
+                    this.numBlue = j;
                     entrance.bluePawn--;
-                    if(Game.isExpert && (position[5][2] == 1 || position[5][5] == 1 || position[5][9] == 1))
+                    if(Game.isExpert && (position[4][2] == 1 || position[4][5] == 1 || position[4][8] == 1))
                         player.numCoin++;
                     break;
                 }
