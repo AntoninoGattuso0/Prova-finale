9b41ea079cc5d8763e320dc2858f6262d60a8af2
AntoninoGattuso0 <antonino.gattuso@mail.polimi.it>
2022-04-22 19:17:59 +0200

    ModificaClassi

--- src/main/java/it/polimi/ingsw/model/Antonio.java
+++ src/main/java/it/polimi/ingsw/model/Antonio.java
@@ -4,14 +4,15 @@ import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Random;
 
-public class Antonio extends CharacterCard{
+public class Antonio {
     protected int numPawnAntonio;
     protected int greenPawn;
     protected int redPawn;
     protected int yellowPawn;
     protected int pinkPawn;
     protected int bluePawn;
-    public void checkStudent(){
+
+    public void checkStudent() {
         Random rnd = new Random();
         ArrayList<String> refillPawn = Game.createArrayPawn();
         if (StudentBag.num > 0) {
@@ -21,7 +22,7 @@ public class Antonio extends CharacterCard{
                 numPawnAntonio++;
                 StudentBag.num--;
                 StudentBag.greenNum--;
-                if(StudentBag.greenNum==0){
+                if (StudentBag.greenNum == 0) {
                     refillPawn.remove(random);
                 }
             } else if (Objects.equals(refillPawn.get(random), "RED")) {
@@ -29,7 +30,7 @@ public class Antonio extends CharacterCard{
                 numPawnAntonio++;
                 StudentBag.num--;
                 StudentBag.redNum--;
-                if(StudentBag.redNum==0){
+                if (StudentBag.redNum == 0) {
                     refillPawn.remove(random);
                 }
             } else if (Objects.equals(refillPawn.get(random), "YELLOW")) {
@@ -37,7 +38,7 @@ public class Antonio extends CharacterCard{
                 numPawnAntonio++;
                 StudentBag.num--;
                 StudentBag.yellowNum--;
-                if(StudentBag.yellowNum==0){
+                if (StudentBag.yellowNum == 0) {
                     refillPawn.remove(random);
                 }
             } else if (Objects.equals(refillPawn.get(random), "PINK")) {
@@ -45,7 +46,7 @@ public class Antonio extends CharacterCard{
                 numPawnAntonio++;
                 StudentBag.num--;
                 StudentBag.pinkNum--;
-                if(StudentBag.pinkNum==0){
+                if (StudentBag.pinkNum == 0) {
                     refillPawn.remove(random);
                 }
             } else if (Objects.equals(refillPawn.get(random), "BLUE")) {
@@ -53,35 +54,35 @@ public class Antonio extends CharacterCard{
                 numPawnAntonio++;
                 StudentBag.num--;
                 StudentBag.blueNum--;
-                if(StudentBag.blueNum==0){
+                if (StudentBag.blueNum == 0) {
                     refillPawn.remove(random);
                 }
             }
         }
     }
-    public void useEffect(ColorPawn colorPawn, Island island){
-            if(Objects.equals(colorPawn.toString(), "GREEN") && this.greenPawn > 0){  //altrimenti cosa succede se la pedina verde non c'è?
-                island.greenPawn++;
-                this.greenPawn--;
-                this.numPawnAntonio--;
-            }else if(Objects.equals(colorPawn.toString(), "RED") && this.redPawn > 0){
-                island.redPawn++;
-                this.redPawn--;
-                this.numPawnAntonio--;
-            }else  if(Objects.equals(colorPawn.toString(), "YELLOW") && this.yellowPawn > 0){
-                island.yellowPawn++;
-                this.yellowPawn--;
-                this.numPawnAntonio--;
-            }else if(Objects.equals(colorPawn.toString(), "PINK") && this.pinkPawn > 0){
-                island.pinkPawn++;
-                this.pinkPawn--;
-                this.numPawnAntonio--;
-            }else if(Objects.equals(colorPawn.toString(), "BLUE") && this.bluePawn > 0){
-                island.bluePawn++;
-                this.bluePawn--;
-                this.numPawnAntonio--;
-            }else {
-                System.out.println("errore colore non presente in entrata");
-            }
+    public void useEffect(ColorPawn colorPawn, Island island) {
+        if (Objects.equals(colorPawn.toString(), "GREEN") && this.greenPawn > 0) {  //altrimenti cosa succede se la pedina verde non c'è?
+            island.greenPawn++;
+            this.greenPawn--;
+            this.numPawnAntonio--;
+        } else if (Objects.equals(colorPawn.toString(), "RED") && this.redPawn > 0) {
+            island.redPawn++;
+            this.redPawn--;
+            this.numPawnAntonio--;
+        } else if (Objects.equals(colorPawn.toString(), "YELLOW") && this.yellowPawn > 0) {
+            island.yellowPawn++;
+            this.yellowPawn--;
+            this.numPawnAntonio--;
+        } else if (Objects.equals(colorPawn.toString(), "PINK") && this.pinkPawn > 0) {
+            island.pinkPawn++;
+            this.pinkPawn--;
+            this.numPawnAntonio--;
+        } else if (Objects.equals(colorPawn.toString(), "BLUE") && this.bluePawn > 0) {
+            island.bluePawn++;
+            this.bluePawn--;
+            this.numPawnAntonio--;
+        } else {
+            System.out.println("errore colore non presente in entrata");
         }
-    }}
+    }
+}
