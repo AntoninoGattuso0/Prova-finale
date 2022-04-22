34b3bac3d79278a682dda7c8186cd4a844c2fcf4
AntoninoGattuso0 <antonino.gattuso@mail.polimi.it>
2022-04-22 20:39:25 +0200

    ModificaClassi

--- src/main/java/it/polimi/ingsw/model/Barbara.java
+++ src/main/java/it/polimi/ingsw/model/Barbara.java
@@ -1,7 +1,18 @@
 package it.polimi.ingsw.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Barbara {
-    public void useEffect(){
-        //devo aspettare che moveprof sia completato per poterlo utilizzare
-    }
+    public void useEffect(int actualPlayer){
+        if(Game.players.get(actualPlayer).diningRoom.numGreen==Game.players.get(ProfTable.greenProf).diningRoom.numGreen)
+                ProfTable.greenProf=actualPlayer;
+        if(Game.players.get(actualPlayer).diningRoom.numRed==Game.players.get(ProfTable.greenProf).diningRoom.numRed)
+            ProfTable.redProf=actualPlayer;
+        if(Game.players.get(actualPlayer).diningRoom.numBlue==Game.players.get(ProfTable.greenProf).diningRoom.numBlue)
+            ProfTable.blueProf=actualPlayer;
+        if(Game.players.get(actualPlayer).diningRoom.numPink==Game.players.get(ProfTable.greenProf).diningRoom.numPink)
+            ProfTable.pinkProf=actualPlayer;
+        if(Game.players.get(actualPlayer).diningRoom.numYellow==Game.players.get(ProfTable.greenProf).diningRoom.numYellow)
+            ProfTable.yellowProf=actualPlayer;
 }
