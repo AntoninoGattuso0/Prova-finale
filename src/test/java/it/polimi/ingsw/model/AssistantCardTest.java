b87e2d49e771383329226816f44475e719a4341f
AntoninoGattuso0 <antonino.gattuso@mail.polimi.it>
2022-04-26 15:13:15 +0200

    ModificaClassi

---
+++ src/test/java/it/polimi/ingsw/model/AssistantCardTest.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.model;
+
+class AssistantCardTest {
+}
--- src/test/java/it/polimi/ingsw/model/CiroTest.java
+++ src/test/java/it/polimi/ingsw/model/CiroTest.java
@@ -2,6 +2,6 @@ package it.polimi.ingsw.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StudentBagTest {
+class CiroTest {
 
 }
