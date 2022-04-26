27a0d7d2a669e9f355f38273944ff31378f2a163
rebecamersam <rebecafrancesca.mersam>
2022-04-26 15:00:56 +0200

    add classes

---
+++ src/test/java/it/polimi/ingsw/model/EntranceTest.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EntranceTest {
+    @Test
+    public void testMovePawnToIsland(){
+
+        Entrance entrance= new Entrance();
+
+
+    }
+}
