8e59833f509c9a521fa3386a6a3ac6c0381b9117
paologambarin <101118508+paologambarin@users.noreply.github.com>
2022-04-29 17:38:18 +0200

    Update classes

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -4,7 +4,7 @@ package it.polimi.ingsw.model;
 import java.util.*;
 
 public class Game {
-    private ArrayList<Player> players = new ArrayList<>(4);
+    protected ArrayList<Player> players = new ArrayList<>(4);
     protected static int totPlayer;
     private ArrayList<Cloud> clouds;
     protected ProfTable profTable;
