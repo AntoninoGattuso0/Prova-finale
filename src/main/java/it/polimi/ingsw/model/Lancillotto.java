4b8711814fe6ceb4fee34d1da64da89322cc3784
paologambarin <101118508+paologambarin@users.noreply.github.com>
2022-04-30 19:49:52 +0200

    Update classes

--- src/main/java/it/polimi/ingsw/model/Lancillotto.java
+++ src/main/java/it/polimi/ingsw/model/Lancillotto.java
@@ -15,6 +15,7 @@ public class Lancillotto extends UseEffect{
         ArrayList<Integer> influence = new ArrayList<>();
         for(i=0; i<Game.totPlayer; i++) influence.add(0);
         for(color=0; color<5; color++) {
+            int nano;
             String s = Game.m.get(color);
             if(s.equals(colorPawn.get(0))) color++;
             n = game.profTable.checkProf(color);
