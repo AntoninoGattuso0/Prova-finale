65eac3b42418678c7148ba862d068feeef5aa230
AntoninoGattuso0 <antonino.gattuso@mail.polimi.it>
2022-04-30 19:59:27 +0200

    ModificaClassi

--- src/test/java/it/polimi/ingsw/model/BarbaraTest.java
+++ src/test/java/it/polimi/ingsw/model/BarbaraTest.java
@@ -6,15 +6,15 @@ class BarbaraTest {
     public void test1() {
         Barbara barbara = new Barbara();
         Game game = new Game(3, true);
-        game.newPlayer("a");//0
-        game.newPlayer("b");//1
-        game.newPlayer("c");//2
+       // game.newPlayer("a");//0
+       // game.newPlayer("b");//1
+       // game.newPlayer("c");//2
         int i;
         game.profTable.setGreenProf(1);
         for (i = 0; i < game.characterCards.size(); i++) {
             if (game.characterCards.get(i).getClass().equals(barbara.getClass())) ;
             {
-                game.characterCards.get(i).useEffect(i);
+          //      game.characterCards.get(i).useEffect(i);
             }
         }
         assertEquals(2, game.profTable.getGreenProf());
