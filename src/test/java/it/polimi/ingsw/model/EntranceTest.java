2a600c956b43adc6d0ac2381ef2a6c22c0b327ea
rebecamersam <rebecafrancesca.mersam>
2022-05-01 23:17:26 +0200

    add classes

--- src/test/java/it/polimi/ingsw/model/EntranceTest.java
+++ src/test/java/it/polimi/ingsw/model/EntranceTest.java
@@ -7,11 +7,14 @@ class EntranceTest {
     @Test
     public void testMovePawnToIsland(){
     Game game= new Game(3,true);
-        Game.newPlayer("ciao",game);
-        System.out.println(Game.players.get(0).entrance.getNumPawn());
-        System.out.println(game.studentBag.getNum());
+        Game.newPlayer("Paolo",game);
+        Game.newPlayer("Rebeca",game);
+        Game.newPlayer("Antonino",game);
 
-
+        System.out.println("Pawns in player's 0 entrance at the begging of the game: "+ Game.players.get(0).entrance.getNumPawn());
+        System.out.println("Pawns in player's 1 entrance at the begging of the game: "+ Game.players.get(1).entrance.getNumPawn());
+        System.out.println("Pawns in player's 2 entrance at the begging of the game: "+ Game.players.get(2).entrance.getNumPawn());
+        System.out.println("Pawns in the student bag: "+ game.studentBag.getNum());
 
     }
 }
--- src/test/java/it/polimi/ingsw/model/ErnestoTest.java
+++ src/test/java/it/polimi/ingsw/model/ErnestoTest.java
@@ -29,14 +29,11 @@ public class ErnestoTest {
         Game.newPlayer(nick1, game);
         Game.newPlayer(nick2, game);
         Game.newPlayer(nick3, game);
-        Island island = new Island();
         Ernesto ernesto = new Ernesto();
         CharacterCard characterCardErnesto = new CharacterCard(ernesto);
         ArrayList<ColorPawn> colorPawn = new ArrayList<>(1);
         colorPawn.add(ColorPawn.GREEN);
         characterCardErnesto.useEffect.useEffect(game, 2, Game.islands.get(0), Game.players.get(0), colorPawn);
-        assertTrue(island.getProhibited());   //credo che il metodo topInfluence di game crei problemi
-        assertEquals(3, ernesto.coinPrice);
-        System.out.println("coinPrice " + ernesto.coinPrice);
+        assertTrue(game.islands.get(0).getProhibited());
     }
 }
