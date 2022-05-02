8fcf992e149af7006a7a193ae15ebd23287edaab
paologambarin <101118508+paologambarin@users.noreply.github.com>
2022-05-02 19:40:50 +0200

    Update classes

--- src/main/java/it/polimi/ingsw/model/Entrance.java
+++ src/main/java/it/polimi/ingsw/model/Entrance.java
@@ -164,8 +164,8 @@ public class Entrance {
                 }
         }
     }
-    public void chooseCloud (Cloud cloud){
-        if(cloud.getNumPawn()!=0 && !checkNum()){      //utile o no il controllo di numPawn? (controller)
+    public void chooseCloud (Cloud cloud, Game game){
+        if(cloud.getNumPawn()!=0 && !checkNum(game)){      //utile o no il controllo di numPawn? (controller)
             setGreenPawn(getGreenPawn() + cloud.getGreenPawn());
             setRedPawn(getRedPawn() + cloud.getRedPawn());
             setYellowPawn(getYellowPawn() + cloud.getYellowPawn());
