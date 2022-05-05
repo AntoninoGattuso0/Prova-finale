7e350e3ff3d2bec2da626809bebb2d58f142de78
rebecamersam <rebecafrancesca.mersam>
2022-05-05 17:12:25 +0200

    add classes

--- src/test/java/it/polimi/ingsw/model/ProfTableTest.java
+++ src/test/java/it/polimi/ingsw/model/ProfTableTest.java
@@ -1,7 +1,20 @@
 package it.polimi.ingsw.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProfTableTest {
+    @Test
+    public void testCheckProf(){
+        Game game = new Game(2, true);
+        game.start(game);
+        Game.newPlayer("Davide", game);
+        Game.newPlayer("Isaac", game);
+        game.players.get(0).diningRoom.setNumGreen(3);
+        game.players.get(1).diningRoom.setNumGreen(1);
+        game.moveProf();
+        assertEquals(0,game.profTable.checkProf(0));
+    }
 
 }
