1a4f4507e74296dd96d2fdb207fd4911defc3637
rebecamersam <rebecafrancesca.mersam>
2022-05-10 15:53:54 +0200

    add classes

--- src/main/java/it/polimi/ingsw/network/Message.java
+++ src/main/java/it/polimi/ingsw/network/Message.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.network;
 
-public class Message extends Exception {
+import java.io.Serializable;
+
+public class Message implements Serializable {
     Object mess;
     public Message(Object message){
         this.mess= message;
