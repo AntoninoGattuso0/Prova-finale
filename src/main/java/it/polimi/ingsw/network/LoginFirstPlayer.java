a8df1e73b2c57a642d94a1734f5468859244736d
rebecamersam <rebecafrancesca.mersam>
2022-05-10 17:05:32 +0200

    add classes

--- src/main/java/it/polimi/ingsw/network/LoginFirstPlayer.java
+++ src/main/java/it/polimi/ingsw/network/LoginFirstPlayer.java
@@ -1,4 +1,27 @@
 package it.polimi.ingsw.network;
 
 public class LoginFirstPlayer extends Message{
+
+    private String nickname;
+    private final int numPlayers;
+    private final boolean isExpert;
+
+
+    public LoginFirstPlayer(String nickname, int numPlayers, boolean isExpert ){
+        this.numPlayers = numPlayers;
+        this.isExpert = isExpert;
+        this.nickname = nickname;
+    }
+
+    public int getNumPlayers(){
+        return numPlayers;
+    }
+
+    public boolean isExpert(){
+        return isExpert;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
 }
