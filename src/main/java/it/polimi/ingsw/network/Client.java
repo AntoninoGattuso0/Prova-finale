b6e194c6b2c71895cd2f82af78abd7a04e1afa07
AntoninoGattuso0 <antonino.gattuso@mail.polimi.it>
2022-05-11 17:58:30 +0200

    ModificaClassi

--- src/main/java/it/polimi/ingsw/network/Client.java
+++ src/main/java/it/polimi/ingsw/network/Client.java
@@ -1,27 +1,136 @@
 package it.polimi.ingsw.network;
-
 import java.io.*;
 import java.net.*;
 
-public class Client implements Runnable {
 
-    Socket mySocket = null;
-    ObjectInputStream in;
-    ObjectOutputStream out;
-    private final int port;
+public class Client implements ClientHandler {
+    private final Socket mySocket;
+    private String userNickname;
+    private volatile boolean connected;
+    private volatile boolean myTurn;
+    private Message message;
+    private final Ping ping;
+    private volatile boolean messageReady;
+    private ObjectInputStream objectInputStream;
+    private ObjectOutputStream objectOutputStream;
 
-    public Client(int port){
-        this.port = port;
+    public Client(Socket socket) {
+        this.mySocket = socket;
+        this.connected = true;
+        this.myTurn = false;
+        this.messageReady = false;
+        this.ping = new Ping();
+    }
+
+    public void setMyTurn(boolean myTurn) {
+        this.myTurn = myTurn;
+    }
+
+    public void sendObject(Object object) {
+        try {
+            objectOutputStream.reset();
+            objectOutputStream.writeObject(object);
+            objectOutputStream.flush();
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+    private void pingClient(){
+        Thread thread= new Thread(()-> {
+            int c = 0;
+            while (connected) {
+                try {
+                    Thread.sleep(50);
+                    c++;
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    if(c>100){
+                        c=0;
+                        objectOutputStream.writeObject(ping);
+                    }
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+    public void closeConnect(){
+        if(userNickname==null){
+            synchronized (this){
+                messageReady=true;
+                message=null;
+                notifyAll();
+            }
+        }
+        connected=false;
+        try{
+            objectOutputStream.close();
+            objectInputStream.close();
+            mySocket.close();
+            System.out.println("SERVER:"+userNickname+"connection close by the server.\n");
+        }catch (IOException e){
+            System.out.println("SERVER: errore closing: "+userNickname+"\n");
+            e.printStackTrace();
+        }
+    }
+    public void readFromClient(){
+        Thread thread= new Thread(()-> {
+            while (connected) {
+                try {
+                    mySocket.setSoTimeout(30000);
+                    Message toServer = null;
+                    try {
+                        toServer = (Message) objectInputStream.readObject();
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    if (!(toServer instanceof Ping)) {
+                        if (myTurn) {
+                            message = toServer;
+                            messageReady = true;
+                            synchronized (this) {
+                                notifyAll();
+                            }
+                        } else {
+                            sendObject(new DisconnectionMessage);
+                        }
+                    }
+                }catch (IOException |NullPointerException|IllegalArgumentException e){
+                    System.out.println("SERVER: "+userNickname+" connection close by the client");
+                    closeConnect();
+                    notifyDisconnection(this);
+                    break;
+                }
+            }
+        });
+        thread.start();
+    }
+    public Message read() {
+        synchronized (this) {
+            while (!messageReady) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        messageReady = false;
+        return message;
     }
 
     public void run() {
         try {
-            Socket mySocket = new Socket(InetAddress.getLocalHost(), port);
-            in = new ObjectInputStream(mySocket.getInputStream());
-            out = new ObjectOutputStream(mySocket.getOutputStream());
+            objectOutputStream= new ObjectOutputStream(mySocket.getOutputStream());
+            objectInputStream = new ObjectInputStream(mySocket.getInputStream());
+            System.out.println("SERVER: new Client Created.");
         } catch (IOException e) {
             e.printStackTrace();
         }
+        readFromClient();
+        pingClient();
     }
-
 }
---
+++ src/main/java/it/polimi/ingsw/network/Ping.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.network;
+
+public class Ping extends Message{
+    private final String message="";
+    public String getMessage(){
+        return message;
+    }
+
+}
