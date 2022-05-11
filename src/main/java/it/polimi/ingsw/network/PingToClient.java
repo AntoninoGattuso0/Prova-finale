9d5f4d3ea9cdaf533c4119ed9892b70161c3bacf
rebecamersam <rebecafrancesca.mersam>
2022-05-11 15:47:56 +0200

    add classes

--- src/main/java/it/polimi/ingsw/network/PingToClient.java
+++ src/main/java/it/polimi/ingsw/network/PingToClient.java
@@ -1,5 +1,14 @@
 package it.polimi.ingsw.network;
 
 public class PingToClient extends ServerToClient{
+    private final String message;
+
+    public PingToClient() {
+        this.message = "";
+    }
+
+    public String getMessage(){
+        return message;
+    }
 
 }
--- src/main/java/it/polimi/ingsw/network/StartTurnMessage.java
+++ src/main/java/it/polimi/ingsw/network/StartTurnMessage.java
@@ -1,7 +1,18 @@
 package it.polimi.ingsw.network;
 
 public class StartTurnMessage extends ServerToClient{
-    private String currentPlayer;
+    private final String currentPlayer;
 
-    //public StartTurnMessage
+    public StartTurnMessage(String currentPlayer){
+        this.currentPlayer = currentPlayer;
+    }
+
+    public String getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public String getMessage(){
+        return currentPlayer + "it's your turn!";
+    }
+
 }
