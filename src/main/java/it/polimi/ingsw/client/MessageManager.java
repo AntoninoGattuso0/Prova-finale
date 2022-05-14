5d940abbfcf3daa345b2c195226c3f7b2d403e49
AntoninoGattuso0 <antonino.gattuso@mail.polimi.it>
2022-05-15 01:13:54 +0200

    ModificaClassi

---
+++ src/main/java/it/polimi/ingsw/client/MessageManager.java
@@ -0,0 +1,40 @@
+package it.polimi.ingsw.client;
+
+import it.polimi.ingsw.network.Message.*;
+
+public class MessageManager {
+    private final View view;
+    public MessageManager(View view){
+        this.view=view;
+    }
+    public void manageInputToclient(Object object){
+        if(object instanceof Ping){
+            return;
+        }
+        if(object instanceof LoginAccepted){
+            view.registerClient((LoginAccepted) object);
+        }else if(object instanceof ChooseCloudMessage){
+            view.
+        }else if(object instanceof WaitMessage){
+            view.
+        }else if(object instanceof StartTurnMessage){
+            view.
+        }else if(object instanceof SetNumPlayers){
+            view.
+        }else if(object instanceof SetNickMessage){
+            view.
+        }else if(object instanceof SetIsExpert){
+            view.
+        }else if(object instanceof MovePawnToDining){
+            view.
+        }else if(object instanceof MovePawnToIsland){
+            view.
+        }else if(object instanceof MoveMotherNatureMessage){
+            view.
+        }else if(object instanceof RemoveClient){
+            view.
+        }else{
+            throw new IllegalArgumentException();
+        }
+    }
+}
--- src/main/java/it/polimi/ingsw/client/View.java
+++ src/main/java/it/polimi/ingsw/client/View.java
@@ -1,4 +1,7 @@
 package it.polimi.ingsw.client;
 
 public class View {
+    public void registerClient(){
+
+    }
 }
