6fc0d11cca94c832653c6260c814a35cd65d29bf
AntoninoGattuso0 <antonino.gattuso@mail.polimi.it>
2022-05-24 19:05:52 +0200

    ModificaClassi

--- src/main/java/it/polimi/ingsw/network/Lobby.java
+++ src/main/java/it/polimi/ingsw/network/Lobby.java
@@ -43,22 +43,7 @@ public class Lobby implements ConnectionObserver {//DA COMPLETARE
     public boolean isLobbySett() {
         return lobbySett;
     }
-    public void addClientToList(ClientHandlerInterface client, Lobby lobby){
-        String nickname;
-        loginUser(client);
-        nickname=client.getUserNickname();
-        if(nickname!=null){
-            if(players.get(0).getNickname()==null){
-                newLobby(client);
-            }else{
-                joiningInLobby(client,lobby);
-            }
-        }
-    }
-    private void newLobby(ClientHandlerInterface client){
-            Lobby lobby=new Lobby();
-            lobby.addClient(client);
-    }
+
     public void addClient(ClientHandlerInterface client){
         synchronized (lock) {
             String nick;
@@ -137,14 +122,6 @@ public class Lobby implements ConnectionObserver {//DA COMPLETARE
 
     private void deregisterConn(ClientHandlerInterface clientHandler) {
     }
-
-    private void joiningInLobby(ClientHandlerInterface client, Lobby lobby){
-            int i;
-            for(i=0;!namePlayer.get(i).equals(client.getUserNickname());i++);
-            if(namePlayer.get(i).equals(client.getUserNickname())){
-                lobby.addClient(client);
-            }
-    }
     public boolean isLobbyOk(){return lobbyOk;}
     //inserisco i player nell'array nomi, e li creo anche nel gioco
     public void loginUser(ClientHandlerInterface loginClient){
@@ -187,6 +164,7 @@ public class Lobby implements ConnectionObserver {//DA COMPLETARE
         System.out.println("SERVER: "+nickname+" is joining!\n");
         loginClient.sendObject(new LoginAcceptedMessage(nickname));
         loginClient.setTurn(false);
+        addClient(loginClient);
     }
     public void endGame(Lobby lobby){
         for(String nickname: namePlayer){
@@ -195,7 +173,7 @@ public class Lobby implements ConnectionObserver {//DA COMPLETARE
         lobby = null;
     }
     public void newGame(){
+        virtualView.setUp();
         controller=new Controller(game,userInput,virtualView,players);
-       // controller.getRoundController()
     }
 }
--- src/main/java/it/polimi/ingsw/network/VirtualView.java
+++ src/main/java/it/polimi/ingsw/network/VirtualView.java
@@ -1,4 +1,5 @@
 package it.polimi.ingsw.network;
+import it.polimi.ingsw.model.*;
 
 import it.polimi.ingsw.network.Message.Message;
 import it.polimi.ingsw.network.Message.StartTurnMessage;
@@ -9,33 +10,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class VirtualView extends ViewObservable /*implements*/ {//DA COMPLETARE
-    private final Map<String, ClientHandlerInterface>  clients= new HashMap<>();
+    private final Map<String, ClientHandlerInterface> clients = new HashMap<>();
     private String actualPlayer;
     private final Object lock;
-    public VirtualView(){
-        this.lock=new Object();
+
+    public VirtualView() {
+        this.lock = new Object();
     }
-    public void addClientInVirtualView(ClientHandlerInterface client, String nick){
-        synchronized (lock){
-            clients.put(nick,client);
+
+    public void addClientInVirtualView(ClientHandlerInterface client, String nick) {
+        synchronized (lock) {
+            clients.put(nick, client);
             lock.notifyAll();
         }
     }
-    public void removeClientInVirtualView(ClientHandlerInterface client, String nick){
-        synchronized (lock){
-            clients.remove(nick,client);
+
+    public void removeClientInVirtualView(ClientHandlerInterface client, String nick) {
+        synchronized (lock) {
+            clients.remove(nick, client);
             lock.notifyAll();
         }
     }
-    public void startTurn(){
-        for(ClientHandlerInterface clientHandler: clients.values()){
+
+    public void startTurn() {
+        for (ClientHandlerInterface clientHandler : clients.values()) {
             clientHandler.sendObject(new StartTurnMessage(actualPlayer));
         }
     }
+
     public void setActualPlayer(String actualPlayer) {
         this.actualPlayer = actualPlayer;
         clients.get(actualPlayer).setTurn(true);
     }
+
     public String getActualPlayer() {
         return actualPlayer;
     }
@@ -44,13 +51,15 @@ public class VirtualView extends ViewObservable /*implements*/ {//DA COMPLETARE
         System.out.println(nick + " is the last Player in lobby. ");
         updateWin(nick);
     }
-    public void updateWin(String nick){
-        System.out.println("The winner is "+ nick+", GameOver");
+
+    public void updateWin(String nick) {
+        System.out.println("The winner is " + nick + ", GameOver");
         sendBroadcast(new WinnerMessage(nick));
     }
-    public void sendBroadcast(Message message){
-        synchronized (lock){
-            for(ClientHandlerInterface clientHandler: clients.values()){
+
+    public void sendBroadcast(Message message) {
+        synchronized (lock) {
+            for (ClientHandlerInterface clientHandler : clients.values()) {
                 clientHandler.sendObject(message);
             }
         }
@@ -65,5 +74,9 @@ public class VirtualView extends ViewObservable /*implements*/ {//DA COMPLETARE
 
     public void endTurn() {
         clients.get(actualPlayer).setTurn(false);
+    }
+
+    public void setUp(Game game) {
+        updateLightGame(game);
     }
 }
