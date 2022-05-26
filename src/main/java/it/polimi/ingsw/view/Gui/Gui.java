c708020a8c6ab464f4037173256b63e07f642d4d
paologambarin <101118508+paologambarin@users.noreply.github.com>
2022-05-26 11:49:27 +0200

    Update classes

--- src/main/java/it/polimi/ingsw/view/Gui/Gui.java
+++ src/main/java/it/polimi/ingsw/view/Gui/Gui.java
@@ -1,4 +1,214 @@
 package it.polimi.ingsw.view.Gui;
 
-public class Gui {
+import it.polimi.ingsw.model.ColorPawn;
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.network.Message.ClientAcceptedMessage;
+import it.polimi.ingsw.network.Message.StartTurnMessage;
+import it.polimi.ingsw.network.Message.UpdateMessage.*;
+import it.polimi.ingsw.observer.ViewObservable;
+import it.polimi.ingsw.view.View;
+
+import java.util.ArrayList;
+
+public class Gui extends ViewObservable implements View {
+    @Override
+    public void startGame() {
+
+    }
+
+    @Override
+    public void loginPlayers() {
+
+    }
+
+    @Override
+    public void displayNick() {
+
+    }
+
+    @Override
+    public void displayNumPlayers(int numPlayers) {
+
+    }
+
+    @Override
+    public void displayIsExpert() {
+
+    }
+
+    @Override
+    public void displayAssistantCard(Player player) {
+
+    }
+
+    @Override
+    public void displayCloud() {
+
+    }
+
+    @Override
+    public void displayIslands() {
+
+    }
+
+    @Override
+    public void displaySchoolBoard() {
+
+    }
+
+    @Override
+    public void displayCharacterCard(Game game) {
+
+    }
+
+    @Override
+    public void updateAssistantCard(AssistantCardUpdateMessage assistant) {
+
+    }
+
+    @Override
+    public void updateCloud(CloudUpdateMessage cloud) {
+
+    }
+
+    @Override
+    public void updateDiningRoom(DiningRoomUpdateMessage object) {
+
+    }
+
+    @Override
+    public void updateEntrance() {
+
+    }
+
+    @Override
+    public void updateIsland(IslandUpdateMessage m) {
+
+    }
+
+    @Override
+    public void updateProfTable() {
+
+    }
+
+    @Override
+    public void updateTowerSpace() {
+
+    }
+
+    @Override
+    public void updateNickname(NicknameUpdateMessage m) {
+
+    }
+
+    @Override
+    public void updateCharacterCard() {
+
+    }
+
+    @Override
+    public void registerClient(ClientAcceptedMessage m) {
+
+    }
+
+    @Override
+    public void updateNumPlayers() {
+
+    }
+
+    @Override
+    public void requestNickname() {
+
+    }
+
+    @Override
+    public void requestNumPlayers() {
+
+    }
+
+    @Override
+    public void requestIsExpert() {
+
+    }
+
+    @Override
+    public void waitOtherPlayers(String object) {
+
+    }
+
+    @Override
+    public void displayWinner(String winner) {
+
+    }
+
+    @Override
+    public void displayNetError() {
+
+    }
+
+    @Override
+    public void displayWrongNickname() {
+
+    }
+
+    @Override
+    public void updatePawnToDining(int numDiningRoom, ArrayList<ColorPawn> arrayPawn) {
+
+    }
+
+    @Override
+    public void updatePawnToIsland(int island, int numPawn, ArrayList<ColorPawn> arrayPawn) {
+
+    }
+
+    @Override
+    public void requestMoveMotherNature(int island) {
+
+    }
+
+    @Override
+    public void updateMoveMotherNature(int island) {
+
+    }
+
+    @Override
+    public void displayWrongTurn() {
+
+    }
+
+    @Override
+    public void displayFetchNameMessage() {
+
+    }
+
+    @Override
+    public void displayTurn(StartTurnMessage object) {
+
+    }
+
+    @Override
+    public void displayResponseMessage(String errorMessage) {
+
+    }
+
+    @Override
+    public void selectCloud(int cloud) {
+
+    }
+
+    @Override
+    public void selectAssistantCard(int assistant) {
+
+    }
+
+    @Override
+    public void updateTowerSpace(TowerSpaceUpdateMessage object) {
+
+    }
+
+    @Override
+    public void updateEntrance(EntranceUpdateMessage object) {
+
+    }
 }
---
+++ src/main/java/it/polimi/ingsw/view/Gui/JavaFXGui.java
@@ -0,0 +1,48 @@
+package it.polimi.ingsw.view.Gui;
+
+import it.polimi.ingsw.network.ClientHandler;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class JavaFXGui extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Gui view = new Gui();
+        ClientHandler clientHandler = new ClientHandler(view);
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/fxml/menu.scene.fxml"));
+        Parent rootlayout = null;
+        try{
+            rootlayout = loader.load();
+        } catch (IOException e){
+            System.exit(1);
+        }
+
+        Scene scene = new Scene(rootlayout);
+        stage.setScene(scene);
+        stage.setWidth(1280d);
+        stage.setHeight(720d);
+        stage.setResizable(false);
+        stage.setMaximized(true);
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("");
+        stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+        stage.setTitle("Eryantis");
+        stage.show();
+
+    }
+
+    @Override
+    public void stop(){
+        Platform.exit();
+        System.exit(0);
+    }
+}
