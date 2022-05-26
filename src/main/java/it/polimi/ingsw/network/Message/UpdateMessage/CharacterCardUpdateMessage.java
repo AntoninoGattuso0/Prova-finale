0a483dcce1a994304da1c0feabd0b689398986d0
rebecamersam <rebecafrancesca.mersam>
2022-05-26 16:48:20 +0200

    added Update Messages

--- src/main/java/it/polimi/ingsw/network/Message/UpdateMessage/CharacterCardUpdateMessage.java
+++ src/main/java/it/polimi/ingsw/network/Message/UpdateMessage/CharacterCardUpdateMessage.java
@@ -1,10 +1,11 @@
 package it.polimi.ingsw.network.Message.UpdateMessage;
 
 import it.polimi.ingsw.model.CharacterCard;
+import it.polimi.ingsw.network.Message.ServerToClient;
 
 import java.util.ArrayList;
 
-public class CharacterCardUpdateMessage {
+public class CharacterCardUpdateMessage extends ServerToClient {
     private static final long serialVersionUID= -311633869817273652L;
     private final ArrayList<CharacterCard> characterCards;
     public CharacterCardUpdateMessage(ArrayList<CharacterCard> characterCards){
