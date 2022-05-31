c66deb9b73a84b1d4574e6a76f20f9742aaacfbc
rebecamersam <rebecafrancesca.mersam>
2022-05-31 17:16:20 +0200

    GUI: login First player

--- src/main/java/it/polimi/ingsw/view/Gui/NumOfPlayerIsExpert.java
+++ src/main/java/it/polimi/ingsw/view/Gui/NumOfPlayerIsExpert.java
@@ -1,4 +1,4 @@
-package it.polimi.ingsw.View.GUI;
+package it.polimi.ingsw.view.GUI;
 
 import it.polimi.ingsw.client.SocketNetworkHandler;
 import it.polimi.ingsw.network.Message.ClientToServer.RequestNickname;
@@ -9,7 +9,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
@@ -17,45 +17,42 @@ import java.awt.*;
 import java.io.IOException;
 import java.util.Objects;
 
-
+/*the first player logged has to insert the number of players
+    and if the game is expert or not
+ */
 public class NumOfPlayerIsExpert{
     private SocketNetworkHandler socketNetworkHandler;
     private Pane rootFXML;
-    private final ImageView readyButton;
+    private final ImageView joinButton;
     private int numPlayer;
-    private boolean expert;
-    private String nickname;
+    private final boolean expert;
+    private final String nickname;
 
-
-    @FXML
-    private Label title;
-    @FXML
-    private Label nick;
-    @FXML
-    private Label numOfPlayer;
-    @FXML
-    private Label isExpert;
     @FXML
     private TextField nickPlayer;
     @FXML
-    private ComboBox<String> numPlayerBox;
+    private ChoiceBox<String> numPlayerBox;
     @FXML
     private CheckBox expertMode;
-    @FXML
-    private CheckBox notExpertMode;
 
     public NumOfPlayerIsExpert(){
         try{
-            rootFXML = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/NumOfPlayerIsExpert.fxml")));
+            rootFXML = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("FXML/NumOfPlayerIsExpert.fxml")));
         }catch(IOException e){
             e.printStackTrace();
         }
 
-        readyButton = (ImageView) rootFXML.lookup("#readyButton");
+        joinButton = (ImageView) rootFXML.lookup("#joinButton");
         ObservableList<String> numPlayers = FXCollections.observableArrayList("2","3","4");
         numPlayerBox.setItems(numPlayers);
 
-        readyButton.setOnMouseClicked(mouseEvent -> {
+        nickname = nickPlayer.getText();
+        socketNetworkHandler.sendMessage(new RequestNickname(nickname));
+
+        if (expertMode.isSelected()) expert = true;
+        else expert = false;
+
+        joinButton.setOnMouseClicked(mouseEvent -> {
             String num = numPlayerBox.getValue();
             switch(num){
                 case "2":
@@ -67,17 +64,8 @@ public class NumOfPlayerIsExpert{
                 case "4":
                     numPlayer = 4;
             }
+            socketNetworkHandler.sendMessage(new RequestNumPlayersIsExpert(numPlayer, expert));
         });
-
-        if(expertMode.isSelected())
-            expert = true;
-        else if(notExpertMode.isSelected())
-            expert = false;
-
-        socketNetworkHandler.sendMessage(new RequestNumPlayersIsExpert(numPlayer, expert));
-
-        nickname = nickPlayer.getText();
-        socketNetworkHandler.sendMessage(new RequestNickname(nickname));
 
     }
 
