471b8ba720e32b2b21345c9d1f61feed334561d6
rebecamersam <rebecafrancesca.mersam>
2022-06-09 01:04:58 +0200

    TableScene

---
+++ src/main/java/it/polimi/ingsw/view/GUI/GameTable.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.view.GUI;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+import java.util.Objects;
+
+
+public class GameTable {
+    private Pane rootFXML;
+
+}
