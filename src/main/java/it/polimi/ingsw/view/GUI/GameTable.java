73c7611d0c20960638a67ebb4f00a06b3a211153
rebecamersam <rebecafrancesca.mersam>
2022-06-13 14:28:26 +0200

    Display starting Scene

--- src/main/java/it/polimi/ingsw/view/GUI/GameTable.java
+++ src/main/java/it/polimi/ingsw/view/GUI/GameTable.java
@@ -34,6 +34,19 @@ public class GameTable {
     private final List<Text> textPink = null;
     private final List<Text> textBlue= null;
 
+    private final List<ImageView> blackTowers = null;
+    private final List<ImageView> whiteTowers = null;
+    private final List<ImageView> greyTowers = null;
+
+    private final List<ImageView> clouds = null;
+    private final List<ImageView> greenPawnClouds = null;
+    private final List<ImageView> redPawnClouds = null;
+    private final List<ImageView> yellowPawnClouds = null;
+    private final List<ImageView> pinkPawnClouds = null;
+    private final List<ImageView> bluePawnClouds = null;
+
+
+
     public GameTable(){
         try{
             rootFXML = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/FXML/GameTable.fxml")));
@@ -63,23 +76,21 @@ public class GameTable {
                 alreadySelected = true;
             }
         }
-    }
 
-
-    //setta all'inizio le pedine presenti invibili
-    //poi se all'inizio c'è la pedina o madre natura la setta visibile
-    //io l'ho fatto poi è da vedere se funziona ;)
-    public void displayInitialGameTable(){
 
         for(int i = 0; i < 12; i++){
             islands.add((ImageView) rootFXML.lookup("island" + i));
             motherNature.add((ImageView) rootFXML.lookup("motherNature" + i));
-        //Aggiunge le immagini delle pedine alle liste del colore corrispondenete
+            //Aggiunge le immagini delle pedine alle liste del colore corrispondenete
             greenPawns.add((ImageView) rootFXML.lookup("green" + i));
             redPawns.add((ImageView) rootFXML.lookup("red" + i));
             yellowPawns.add((ImageView) rootFXML.lookup("yellow" + i));
             pinkPawns.add((ImageView) rootFXML.lookup("pink" + i));
             bluePawns.add((ImageView) rootFXML.lookup("blue" + i));
+            //aggiunge le torri del colore corrispondente
+            blackTowers.add((ImageView) rootFXML.lookup("blackTower" + i));
+            whiteTowers.add((ImageView) rootFXML.lookup("whiteTower" + i));
+            greyTowers.add((ImageView) rootFXML.lookup("greyTowers" + i));
 
             textGreen.add((Text) rootFXML.lookup("textGreen" + i));
             textRed.add((Text) rootFXML.lookup("textRed" + i));
@@ -87,25 +98,30 @@ public class GameTable {
             textPink.add((Text) rootFXML.lookup("textPink" + i));
             textBlue.add((Text) rootFXML.lookup("textBlue" + i));
         }
+
         //setta tutte le pedine e madre natura invisibili all'inizio
         for(ImageView greenPawn : greenPawns)
             greenPawn.setVisible(false);
-
-         for(ImageView redPawn : redPawns)
+        for(ImageView redPawn : redPawns)
             redPawn.setVisible(false);
-
-         for(ImageView yellowPawn : yellowPawns)
+        for(ImageView yellowPawn : yellowPawns)
             yellowPawn.setVisible(false);
-
-         for(ImageView pinkPawn : pinkPawns)
+        for(ImageView pinkPawn : pinkPawns)
             pinkPawn.setVisible(false);
-
-         for(ImageView pinkPawn : pinkPawns)
+        for(ImageView pinkPawn : pinkPawns)
             pinkPawn.setVisible(false);
 
         for(ImageView mNature : motherNature)
             mNature.setVisible(false);
+        //setta le torri non visibili
+        for(ImageView blackTower : blackTowers)
+            blackTower.setVisible(false);
+        for(ImageView whiteTower : whiteTowers)
+            whiteTower.setVisible(false);
+        for(ImageView greyTower : greyTowers)
+            greyTower.setVisible(false);
 
+        //setta il testo delle pedine non visibili
         for(Text greenText : textGreen)
             greenText.setVisible(false);
         for(Text redText : textRed)
@@ -117,7 +133,38 @@ public class GameTable {
         for(Text blueText : textBlue)
             blueText.setVisible(false);
 
+        for(int i = 0; i < 4; i++){
+            clouds.add((ImageView) rootFXML.lookup("cloud" + i));
+            clouds.get(i).setVisible(false);
+        }
+        for(int i = 0; i < lightGame.getNumPlayers(); i++)
+            clouds.get(i).setVisible(true);
 
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 3; j++){
+                greenPawnClouds.add((ImageView) rootFXML.lookup("greenCloud" + i + j));
+                redPawnClouds.add((ImageView) rootFXML.lookup("redCloud" + i + j));
+                yellowPawnClouds.add((ImageView) rootFXML.lookup("yellowCloud" + i + j));
+                pinkPawnClouds.add((ImageView) rootFXML.lookup("pinkCloud" + i + j));
+                bluePawnClouds.add((ImageView) rootFXML.lookup("blueCloud" + i + j));
+            }
+        }
+        for(int i = 0; i < 12; i++){
+            greenPawnClouds.get(i).setVisible(false);
+            redPawnClouds.get(i).setVisible(false);
+            yellowPawnClouds.get(i).setVisible(false);
+            pinkPawnClouds.get(i).setVisible(false);
+            bluePawnClouds.get(i).setVisible(false);
+        }
+
+
+    }
+
+
+    //settato nel costruttore le pedine presenti invibili all'inizio
+    //poi quando vengono randomizzate le pedine  e MN all'inizio del gioco, vengono settate su visibiili
+    //io l'ho fatto poi è da vedere se funziona ;)
+    public void displayInitialGameTable(){
          for(int i = 0; i < 12; i++){
              if(lightGame.getIslands().get(i).getGreenPawn()>0){
                  greenPawns.get(i).setVisible(true);
