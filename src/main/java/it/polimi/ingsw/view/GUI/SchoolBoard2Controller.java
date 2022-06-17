251f974cfd2e2296da43e8671c70e550f6c3eb2f
rebecamersam <rebecafrancesca.mersam>
2022-06-17 14:26:24 +0200

    SchoolBoard fxml

--- src/main/java/it/polimi/ingsw/view/GUI/SchoolBoard2Controller.java
+++ src/main/java/it/polimi/ingsw/view/GUI/SchoolBoard2Controller.java
@@ -1,5 +1,107 @@
 package it.polimi.ingsw.view.GUI;
 
-public class SchoolBoard2Controller {
+import javafx.scene.input.MouseEvent;
 
+public class SchoolBoard2Controller {
+    //colorepedina, numeroSchoolBoard, numero posizione della pedina sull'entrata
+    //POSIZIONE PEDINE ENTRATA:
+    //0 2 4 6 8
+    //  1 3 5 7
+    public void green20Select(MouseEvent mouseEvent) {
+    }
+    public void green21Select(MouseEvent mouseEvent) {
+    }
+    public void green22Select(MouseEvent mouseEvent) {
+    }
+    public void green23Select(MouseEvent mouseEvent) {
+    }
+    public void green24Select(MouseEvent mouseEvent) {
+    }
+    public void green25Select(MouseEvent mouseEvent) {
+    }
+    public void green26Select(MouseEvent mouseEvent) {
+    }
+    public void green27Select(MouseEvent mouseEvent) {
+    }
+    public void green28Select(MouseEvent mouseEvent) {
+    }
+
+
+    public void red20Select(MouseEvent mouseEvent) {
+    }
+    public void red21Select(MouseEvent mouseEvent) {
+    }
+    public void red22Select(MouseEvent mouseEvent) {
+    }
+    public void red23Select(MouseEvent mouseEvent) {
+    }
+    public void red24Select(MouseEvent mouseEvent) {
+    }
+    public void red25Select(MouseEvent mouseEvent) {
+    }
+    public void red26Select(MouseEvent mouseEvent) {
+    }
+    public void red27Select(MouseEvent mouseEvent) {
+    }
+    public void red28Select(MouseEvent mouseEvent) {
+    }
+
+
+    public void yellow20Select(MouseEvent mouseEvent) {
+    }
+    public void yellow21Select(MouseEvent mouseEvent) {
+    }
+    public void yellow22Select(MouseEvent mouseEvent) {
+    }
+    public void yellow23Select(MouseEvent mouseEvent) {
+    }
+    public void yellow24Select(MouseEvent mouseEvent) {
+    }
+    public void yellow25Select(MouseEvent mouseEvent) {
+    }
+    public void yellow26Select(MouseEvent mouseEvent) {
+    }
+    public void yellow27Select(MouseEvent mouseEvent) {
+    }
+    public void yellow28Select(MouseEvent mouseEvent) {
+    }
+
+
+    public void pink20Select(MouseEvent mouseEvent) {
+    }
+    public void pink21Select(MouseEvent mouseEvent) {
+    }
+    public void pink22Select(MouseEvent mouseEvent) {
+    }
+    public void pink23Select(MouseEvent mouseEvent) {
+    }
+    public void pink24Select(MouseEvent mouseEvent) {
+    }
+    public void pink25Select(MouseEvent mouseEvent) {
+    }
+    public void pink26Select(MouseEvent mouseEvent) {
+    }
+    public void pink27Select(MouseEvent mouseEvent) {
+    }
+    public void pink28Select(MouseEvent mouseEvent) {
+    }
+
+    public void blue20Select(MouseEvent mouseEvent) {
+    }
+    public void blue21Select(MouseEvent mouseEvent) {
+    }
+    public void blue22Select(MouseEvent mouseEvent) {
+    }
+    public void blue23Select(MouseEvent mouseEvent) {
+    }
+    public void blue24Select(MouseEvent mouseEvent) {
+    }
+    public void blue25Select(MouseEvent mouseEvent) {
+    }
+    public void blue26Select(MouseEvent mouseEvent) {
+    }
+    public void blue27Select(MouseEvent mouseEvent) {
+    }
+    public void blue28Select(MouseEvent mouseEvent) {
+    }
 }
