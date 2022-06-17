e5062a8c512f6ca7ddbd5d31b5d85442d9564185
rebecamersam <rebecafrancesca.mersam>
2022-06-17 12:35:55 +0200

    fxml

---
+++ src/main/java/it/polimi/ingsw/view/GUI/SchoolBoard0Controller.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.view.GUI;
+
+import javafx.scene.input.MouseEvent;
+
+public class SchoolBoard0Controller {
+    public void green00Select(MouseEvent mouseEvent) {
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/view/GUI/SchoolBoard3Controller.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.view.GUI;
+
+import javafx.scene.input.MouseEvent;
+
+public class SchoolBoard3Controller {
+
+}
