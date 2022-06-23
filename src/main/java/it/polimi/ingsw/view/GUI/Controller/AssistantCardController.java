e7cdaf627649b24d961ba738e37091a1f1a3d1fc
AntoninoGattuso0 <antonino.gattuso@mail.polimi.it>
2022-06-23 10:44:14 +0200

    ModificaClassi

--- src/main/java/it/polimi/ingsw/view/GUI/Controller/AssistantCardController.java
+++ src/main/java/it/polimi/ingsw/view/GUI/Controller/AssistantCardController.java
@@ -70,32 +70,42 @@ public class AssistantCardController {
     }
         public void assistant1Select () {
             gui.getSocketNetworkHandler().sendMessage(new ChooseAssistantCardMessage(0));
+            gui.assistantSelected();
         }
         public void assistant2Select () {
             gui.getSocketNetworkHandler().sendMessage(new ChooseAssistantCardMessage(1));
+            gui.assistantSelected();
         }
         public void assistant3Select () {
             gui.getSocketNetworkHandler().sendMessage(new ChooseAssistantCardMessage(2));
+            gui.assistantSelected();
         }
         public void assistant4Select () {
             gui.getSocketNetworkHandler().sendMessage(new ChooseAssistantCardMessage(3));
+            gui.assistantSelected();
         }
         public void assistant5Select () {
             gui.getSocketNetworkHandler().sendMessage(new ChooseAssistantCardMessage(4));
+            gui.assistantSelected();
         }
         public void assistant6Select () {
             gui.getSocketNetworkHandler().sendMessage(new ChooseAssistantCardMessage(5));
+            gui.assistantSelected();
         }
         public void assistant7Selected () {
             gui.getSocketNetworkHandler().sendMessage(new ChooseAssistantCardMessage(6));
+            gui.assistantSelected();
         }
         public void assistant8Select () {
             gui.getSocketNetworkHandler().sendMessage(new ChooseAssistantCardMessage(7));
+            gui.assistantSelected();
         }
         public void assistant9Select () {
             gui.getSocketNetworkHandler().sendMessage(new ChooseAssistantCardMessage(8));
+            gui.assistantSelected();
         }
         public void assistant10Select () {
             gui.getSocketNetworkHandler().sendMessage(new ChooseAssistantCardMessage(9));
+            gui.assistantSelected();
         }
     }
