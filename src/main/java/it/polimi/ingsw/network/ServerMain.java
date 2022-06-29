db02eaadb43960a3b76d5ea7415b122c61c24cce
AntoninoGattuso0 <antonino.gattuso@mail.polimi.it>
2022-06-29 16:10:18 +0200

    ModificaClassi

--- src/main/java/it/polimi/ingsw/network/ServerMain.java
+++ src/main/java/it/polimi/ingsw/network/ServerMain.java
@@ -3,16 +3,33 @@ package it.polimi.ingsw.network;
 import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
 
 public class ServerMain {
     /**
      * start the Server and print the Address
+     *
      * @see Server
      */
     public static void main(String[] args) throws IOException {
-        Server server=new Server(4000);
-        InetAddress address= InetAddress.getByAddress(Inet4Address.getLocalHost().getAddress());
-        System.out.println("l'indirizzo ip del server è: " + address);
+        Server server = new Server(4000);
+        InetAddress address = InetAddress.getByAddress(Inet4Address.getLocalHost().getAddress());
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface networkInterface = interfaces.nextElement();
+            // drop inactive
+            if (!networkInterface.isUp())
+                continue;
+            Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
+            while (addresses.hasMoreElements()) {
+                InetAddress addr = addresses.nextElement();
+                System.out.println(String.format("NetInterface: name '%s', ip: %s",
+                        networkInterface.getDisplayName(), addr.getHostAddress()));
+            }
+        }
+        //System.out.println("l'indirizzo ip del server è: " + address);
         server.start();
     }
 }
